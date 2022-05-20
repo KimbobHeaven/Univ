b4fef94e9a3e139d7c1448ae9671b1bb6c093d52
KimbobHeaven <68644600+KimbobHeaven@users.noreply.github.com>
2022-05-20 23:06:36 +0900

    Create servivors.h

---
+++ Data Structure/Assignment 01/servivors.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class servivors {
+public:
+	servivors();
+	servivors(int, int);
+	void setFst(int);
+	void setSnd(int);
+	int getFst();
+	int getSnd();
+private:
+	int fstServ;
+	int sndServ;
+};
